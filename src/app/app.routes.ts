e89320d345d183cb853e876dc2adf8d5eecf59da
Michael Yapi <myapi@edane.org>
2025-12-03 17:44:34 +0000

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,43 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./core/layout/public-layout/public-layout').then(m => m.PublicLayout),
+        children: [
+            {
+                path: '',
+                redirectTo: 'home',
+                pathMatch: 'full'
+            },
+            {
+                path: 'home',
+                loadComponent: () => import('./features/portail/home/home').then(m => m.Home),
+                title: 'Accueil'
+            },
+            // {
+            //     path: 'annonces',
+            //     children: [
+            //         {
+            //             path: '',
+            //             loadComponent: () => import('./pages/public/annonces/list/annonces-list.component').then(m => m.AnnoncesListComponent),
+            //             title: 'Toutes les annonces'
+            //         }
+            //     ]
+            // }
+        ]
+    },
+
+    // {
+    //     path: '404',
+    //     loadComponent: () => import('./pages/errors/not-found/not-found.component').then(m => m.NotFoundComponent),
+    //     title: 'Page non trouvée'
+    // },
+
+
+
+
+
+
+
+];
